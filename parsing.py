c066c7bb0a94d48d29b9c7a47c9ee7bb0988df31
apumaparna <aparnakumargs@gmail.com>
2024-04-15 13:37:59 -0400

    lexer class

--- parsing.py
+++ parsing.py
@@ -1,6 +1,10 @@
 # Import any necessary libraries
 import ply.lex as lex 
 
+class MyLexer(object):
+    # This is the class that will build the Lexer 
+    pass 
+
 # Define your parsing function(s) here
 
 def main():
