bc9961250b14ef570d46b838962e274e851ffe07
YahyaElGawady <33075508+YahyaElGawady@users.noreply.github.com>
2024-04-15 14:27:54 -0400

    C file formatting
    We worked on creating alot of the back bone for the c file. A lot of this is up for change as we learn more about the parsing.

--- pythonToC.py
+++ pythonToC.py
@@ -1,9 +1,31 @@
 # Import any necessary modules here
 
 # Define your functions here
-
+def set_up (args, file_name):
+    file = open(file_name + ".c", "a")
+    file.write("#include <stdio.h>\n")
+    file.write("#include <cuda_runtime.h>\n\n")
+    file.write("__global__ void " + file_name + "(int [][] args) {\n")
+    # parse through the function
+    file.write("}\n\n__host__\n")
+    file.write("int main() {\n")
+    file.write("const int THREADS_PER_BLOCK = 256, BLOCKS = 3;\n\n")
+    # convert args to c
+    # convert args from device to host
+    file.write(file_name + "<<<BLOCKS,THREADS_PER_BLOCK>>>(args);\n")
+    file.write("cudaDeviceSynchronize();\n")
+    # copy from host to device
+    # free all the variables
+    file.write("return 0;\n}")
+def args_to_c:
+    #convert python args to c
+    #numpy
 def main():
-    # Your main code logic goes here
+
+
+
+
+
     pass
 
 if __name__ == "__main__":
