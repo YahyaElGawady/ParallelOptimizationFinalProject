abdf7af628721e95923a3db3fc4b5881823ce9d9
ruhesolomon <68287295+ruhesolomon@users.noreply.github.com>
2024-04-23 12:06:48 -0400

    Update pythonToC.py

--- pythonToC.py
+++ pythonToC.py
@@ -51,6 +51,31 @@ def numpy_dot_product_to_c(a, b, result):
     b_name = get_var_name(b)
     result_name = get_var_name(result)
     return f"dotArraysHelper({a_name}, {b_name}, {result_name})"
+
+def set_up_host(args, filen_name, matrix_dim):
+    file = open(file_name + ".c", "a")
+    file.write("    float *h_input, *h_output;\n")
+    file.write(f"    h_input = (float*)malloc({matrix_dim} * {matrix_dim} * sizeof(float));\n")
+    file.write(f"    h_output = (float*)malloc({matrix_dim} * {matrix_dim} * sizeof(float));\n")
+    
+    file.write("    float *d_input, *d_output;\n")
+    file.write(f"    cudaMalloc(&d_input, {matrix_dim} * {matrix_dim} * sizeof(float));\n")
+    file.write(f"    cudaMalloc(&d_output, {matrix_dim} * {matrix_dim} * sizeof(float));\n\n")
+
+    file.write(f"    cudaMemcpy(d_input, h_input, {matrix_dim} * {matrix_dim} * sizeof(float), cudaMemcpyHostToDevice);\n\n")
+    file.write("    THREADS_PER_BLOCK = 256 ;\n")
+    file.write("    BlOCKS_PER_GRID = 256 ;\n")
+    file.write("    CArraysHelper<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>>(args);\n")
+    file.write("    cudaDeviceSynchronize();\n\n")
+
+    file.write(f"    cudaMemcpy(h_output, d_output, {matrix_dim} * {matrix_dim} * sizeof(float), cudaMemcpyDeviceToHost);\n\n")
+
+    file.write("    cudaFree(d_input);\n")
+    file.write("    cudaFree(d_output);\n")
+    file.write("    free(h_input);\n")
+    file.write("    free(h_output);\n\n")
+    
+
 def main():
     pass
 
