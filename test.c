8382de0acc5457fe26668f6d52f0eccfb96bce48
Khadija <khadijasiddiqui183@gmail.com>
2024-04-27 17:41:17 -0400

    trying to fix errs

--- test.c
+++ test.c
@@ -38,3 +38,23 @@ void test_kernel(float * d_input, float * d_output, const int matrix_dim){
 }
 __host__
  int main() {
+//%%cuda_group_save -n run.cu -g default
+#include <stdio.h>
+#include "util.h"
+#include "help_file.c"
+
+__global__
+void test_kernel(float * d_input, float * d_output, const int matrix_dim){
+}
+__host__
+ int main() {
+//%%cuda_group_save -n run.cu -g default
+#include <stdio.h>
+#include "util.h"
+#include "help_file.c"
+
+__global__
+void test_kernel(float * d_input, float * d_output, const int matrix_dim){
+}
+__host__
+ int main() {
