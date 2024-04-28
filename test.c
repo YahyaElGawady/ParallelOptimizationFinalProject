002bf2a6dcdf0606d3537492e293db450c2a4b37
apumaparna <aparnakumargs@gmail.com>
2024-04-28 15:48:04 -0400

    making changes to the python testing script to fix some errors

--- test.c
+++ test.c
@@ -210,3 +210,24 @@ void test_kernel(float * d_input, float * d_output, const int matrix_dim){
 
 __global__
 void test_kernel(float * d_input, float * d_output, const int matrix_dim){
+//%%cuda_group_save -n run.cu -g default
+#include <stdio.h>
+#include "util.h"
+#include "help_file.c"
+
+__global__
+void test_kernel(float * d_input, float * d_output, const int matrix_dim){
+//%%cuda_group_save -n run.cu -g default
+#include <stdio.h>
+#include "util.h"
+#include "help_file.c"
+
+__global__
+void test_kernel(float * d_input, float * d_output, const int matrix_dim){
+//%%cuda_group_save -n run.cu -g default
+#include <stdio.h>
+#include "util.h"
+#include "help_file.c"
+
+__global__
+void test_kernel(float * d_input, float * d_output, const int matrix_dim){
